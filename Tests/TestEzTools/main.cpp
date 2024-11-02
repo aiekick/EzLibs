f117e282ac832b262978f589a4b501a5a5e602e4
aiekick <aiekick@funparadigm.com>
2024-11-02 06:11:04 +0100

    -

--- Tests/TestEzTools/main.cpp
+++ Tests/TestEzTools/main.cpp
@@ -10,5 +10,5 @@ int main(int argc, char** argv) {
         return TestEzTools(argv[1]) ? 0 : 1;
     }
     // User testing
-    return TestEzTools("TestEzCron_TimeCheck_Valid") ? 0 : 1;
+    return TestEzTools("TestEzCron_Format_ManyErrors_1") ? 0 : 1;
 }
